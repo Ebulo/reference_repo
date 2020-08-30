3500906b3d2f569b8ae24d5e22d405014e43cbea
Saswat Sachin Mishra <ssm13092013@gmail.com>
2020-08-30 16:46:49 +0530

    Add files via upload

---
+++ js/product.js
@@ -0,0 +1,20 @@
+function change()
+{
+    var ProductImg = document.getElementById("ProductImg");
+    ProductImg.src = 'Images/images/gallery-1.jpg';
+}
+function change1()
+{
+    var ProductImg = document.getElementById("ProductImg");
+    ProductImg.src = 'Images/images/gallery-2.jpg';
+}
+function change2()
+{
+    var ProductImg = document.getElementById("ProductImg");
+    ProductImg.src = 'Images/images/gallery-3.jpg';
+}
+function change3()
+{
+    var ProductImg = document.getElementById("ProductImg");
+    ProductImg.src = 'Images/images/gallery-4.jpg';
+}
